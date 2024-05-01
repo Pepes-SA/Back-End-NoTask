5ee0d958fd05feba66676e961482c473cb8fec46
Jeanpaul417 <juanpabloint@gmail.com>
2024-05-01 13:23:44 -0500

    Error corregido en NotesControllers

--- Controllers/NotesControllers.cs
+++ Controllers/NotesControllers.cs
@@ -1,18 +1,20 @@
-using Microsoft.AspNetCore.Components;
-using BackEndNoTask.Data;
 using Microsoft.AspNetCore.Mvc;
+using BackEndNoTask.Data;
 
-[Route("api/[controller]")]
-[ApiController]
-
-
-
-  public class NotesController : ControllerBase
-  {
-    private readonly BaseContext _context;
-
-    public NotesController(BaseContext context)
+namespace BackEndNoTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
     {
-      _context = context;
+        private readonly BaseContext _context;
+
+        public NotesController(BaseContext context)
+        {
+            _context = context;
+        }
+
+        // Aquí podrías agregar acciones (endpoints) para manejar solicitudes HTTP,
+        // por ejemplo, GET, POST, PUT, DELETE, etc.
     }
-  }
+}
