189645cace160982f2354f8a50132e73a01c6e95
Jeanpaul417 <juanpabloint@gmail.com>
2024-04-30 20:13:56 -0500

    prueba subida

--- obj/Debug/net8.0/Back-End-NoTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/Back-End-NoTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55598c4ac40929b668264d79284469737fd6a927")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
