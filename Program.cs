c3e9ea447929f8792e4787513d32223e142f49ad
Jeanpaul417 <juanpabloint@gmail.com>
2024-05-02 16:57:56 -0400

    Conexion base de datos full

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllers();
 
 builder.Services.AddDbContext<BaseContext>(Options =>
                             Options.UseMySql(
@@ -14,6 +13,7 @@ builder.Services.AddDbContext<BaseContext>(Options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
