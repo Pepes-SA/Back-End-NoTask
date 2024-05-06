2d2d1aac2a56b48e9bf1d54ea3b4e77c1132bca7
Andres <142694258+patoElDestripador@users.noreply.github.com>
2024-05-06 19:52:06 +0000

    se config el post del back

--- Controllers/NoteCategorysControllers.cs
+++ Controllers/NoteCategorysControllers.cs
@@ -5,104 +5,135 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BackEndNoTask.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class NoteCategorysController : Controller
+  [Route("api/[controller]")]
+  [ApiController]
+  public class NoteCategorysController : Controller
+  {
+    private readonly BaseContext _context;
+
+    public NoteCategorysController(BaseContext context)
     {
-        private readonly BaseContext _context;
+      _context = context;
+    }
 
-        public NoteCategorysController(BaseContext context)
-        {
-            _context = context;
-        }
+    //Listado de Notas
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<NoteCategory>>> GetNoteCategorys()
+    {
+      return await _context.NoteCategorys.ToListAsync();
+    }
 
-        //Listado de Notas
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<NoteCategory>>> GetNoteCategorys()
-        {
-          return await _context.NoteCategorys.ToListAsync();
-        }
+    // Detalles 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<NoteCategory>> GetNoteCategory(int id)
+    {
+      var note = await _context.NoteCategorys.FindAsync(id);
 
-        // Detalles 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<NoteCategory>> GetNoteCategory(int id)
-        {
-          var note = await _context.NoteCategorys.FindAsync(id);
+      if (note == null)
+      {
+        return NotFound();
+      }
+      return note;
+    }
 
-          if (note == null)
-          {
-            return NotFound();
-          }
-          return note;
-        }
+    //Crear una nota
+    [HttpPost]
+    public async Task<ActionResult<NoteCategory>> PostNoteCategory([Bind("Name")] NoteCategory data)
+    {
+      NoteCategory category = new NoteCategory()
+      {
+        Name = data.Name,
+        CreationDate = DateTime.Now,
+        Status = "Activo"
+      };
+      _context.NoteCategorys.Add(category);
+      await _context.SaveChangesAsync();
+      return CreatedAtAction("GetNoteCategory", new { id = category.Id }, category);
+    }
 
-        //Crear una nota
-        [HttpPost]
-        public async Task<ActionResult<NoteCategory>> PostNoteCategory(NoteCategory note)
-        {
-          _context.NoteCategorys.Add(note);
-          await _context.SaveChangesAsync();
+    //Eliminar una nota
 
-          return CreatedAtAction("GetPerson", new { id = note.Id }, note);
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteNoteCategory(int id)
+    {
+      var note = await _context.NoteCategorys.FindAsync(id);
+      if (note == null)
+      {
+        return NotFound();
+      }
 
-        }
-       
-       //Eliminar una nota
+      _context.NoteCategorys.Remove(note);
+      await _context.SaveChangesAsync();
+      return NoContent();
+    }
 
-       [HttpDelete("{id}")]
-       public async Task<IActionResult> DeleteNoteCategory(int id)
-       { 
-        var note = await _context.NoteCategorys.FindAsync(id);
-        if (note == null)
+    //Actualizar nota
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutNoteCategory(int id, NoteCategory updatedNote)
+    {
+      if (id != updatedNote.Id)
+      {
+        return BadRequest();
+      }
+
+      var note = await _context.NoteCategorys.FindAsync(id);
+      if (note == null)
+      {
+        return NotFound();
+      }
+      note.Name = updatedNote.Name;
+      note.CreationDate = updatedNote.CreationDate;
+      note.Status = updatedNote.Status;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!NoteExists(id))
         {
           return NotFound();
         }
-
-        _context.NoteCategorys.Remove(note);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-       }
-
-       //Actualizar nota
-       [HttpPut("{id}")]
-       public async Task<IActionResult> PutNoteCategory(int id, NoteCategory updatedNote)
-       {
-        if (id != updatedNote.Id)
+        else
         {
-          return BadRequest();
+          throw;
         }
-
-        var note = await _context.NoteCategorys.FindAsync(id);
-        if (note == null)
-        {
-        return NotFound();
-        }
-        note.Name = updatedNote.Name;
-        note.CreationDate = updatedNote.CreationDate;
-        note.Status = updatedNote.Status;
-
-        try
-        {
-          await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-          if (!NoteExists(id))
-          {
-            return NotFound();
-          }
-          else
-          {
-            throw;
-          }
-        }
+      }
       return NoContent();
-       }
-
-       private bool NoteExists(int id)
-       {
-      return _context.NoteCategorys.Any(e => e.Id == id);
-       }
     }
+
+    [HttpPut("{id}/{status}")]
+    public async Task<IActionResult> ChangeStatusCategory(int id, string status)
+    {
+      var note = await _context.NoteCategorys.FindAsync(id);
+      if (note == null)
+      {
+        return NotFound();
+      }
+      note.Status = status;
+
+      try
+      {
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!NoteExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    private bool NoteExists(int id)
+    {
+      return _context.NoteCategorys.Any(e => e.Id == id);
+    }
+  }
 }
--- obj/Debug/net8.0/Back-End-NoTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/Back-End-NoTask.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e9ea447929f8792e4787513d32223e142f49ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c01a9dc46e7645999d032cbb0ec0b321d3ca98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-End-NoTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
